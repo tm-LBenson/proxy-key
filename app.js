8c1ba2d83a5054d1b93b83ba22903184e7601d99
Lewis Benson <tm.lewisbenson@gmail.com>
2024-06-07 14:17:51 -0400

    add enpoints

---
+++ app.js
@@ -0,0 +1,27 @@
+// express server
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Server is running' });
+});
+
+app.post('/get-key', (req, res) => {
+  if (req.body.secret === process.env.SECRET) {
+    res.send({ key: process.env.KEY });
+  } else {
+    res.status(401).send({ message: 'Unauthorized' });
+  }
+});
+
+app.use((req, res) => {
+  res.status(404).send({ message: 'Endpoint not found' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
